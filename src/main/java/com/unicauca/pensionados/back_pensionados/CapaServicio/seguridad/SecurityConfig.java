9e92774953bc7ff41813120fae0c68dfa0e20ce3
JuanCarlosManquillo <jucmanquillo@unicauca.edu.co>
2025-10-05 16:44:20 -0500

    CORS config

--- src/main/java/com/unicauca/pensionados/back_pensionados/CapaServicio/seguridad/SecurityConfig.java
+++ src/main/java/com/unicauca/pensionados/back_pensionados/CapaServicio/seguridad/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+            configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173","http://181.32.123.173:5173/","http://181.32.123.173:3000/"));
             configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS" , "PATCH"));
             configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
             configuration.setAllowCredentials(true);
