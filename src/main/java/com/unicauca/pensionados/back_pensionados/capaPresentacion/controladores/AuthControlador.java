20dfaff385cc36aa2734778b00620a7255da24cb
Yesid Obando <93365222+Yesid812@users.noreply.github.com>
2025-11-10 16:47:05 -0500

    Enable CORS in AuthControlador
    Added CrossOrigin annotation to allow requests from any origin.

--- src/main/java/com/unicauca/pensionados/back_pensionados/capaPresentacion/controladores/AuthControlador.java
+++ src/main/java/com/unicauca/pensionados/back_pensionados/capaPresentacion/controladores/AuthControlador.java
@@ -28,6 +28,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 @Tag(name = "Auth Controlador", description = "Operaciones de autenticación de usuarios")
+@CrossOrigin(origins = "*")
 public class AuthControlador {
 
     private final AuthService authService;
@@ -56,4 +57,5 @@ public class AuthControlador {
     }
     
     
+
 }
